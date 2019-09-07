4730f16def58ae219821ca71268055a57ad7fe80
hsuan-chen lin <hsuanchen@outlook.com>
2019-09-08 00:57:39 +0800

    finish lab1 Part I: Map/Reduce input and output

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -1,5 +1,12 @@
 package mapreduce
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"sort"
+)
+
 func doReduce(
 	jobName string, // the name of the whole MapReduce job
 	reduceTask int, // which reduce task this is
@@ -12,11 +19,17 @@ func doReduce(
 	// files for the task, sort the intermediate key/value pairs by key,
 	// call the user-defined reduce function (reduceF) for each key, and
 	// write reduceF's output to disk.
-	//
+
 	// You'll need to read one intermediate file from each map task;
 	// reduceName(jobName, m, reduceTask) yields the file
 	// name from map task m.
 	//
+
+
+	//for _, k := range keys {
+	//	fmt.Println("Key:", k, "Value:", m[k])
+	//}
+
 	// Your doMap() encoded the key/value pairs in the intermediate
 	// files, so you will need to decode them. If you used JSON, you can
 	// read and decode by creating a decoder and repeatedly calling
@@ -28,7 +41,7 @@ func doReduce(
 	// reduceF() is the application's reduce function. You should
 	// call it once per distinct key, with a slice of all the values
 	// for that key. reduceF() returns the reduced value for that key.
-	//
+
 	// You should write the reduce output as JSON encoded KeyValue
 	// objects to the file named outFile. We require you to use JSON
 	// because that is what the merger than combines the output
@@ -36,12 +49,44 @@ func doReduce(
 	// JSON -- it is just the marshalling format we chose to use. Your
 	// output code will look something like this:
 	//
-	// enc := json.NewEncoder(file)
-	// for key := ... {
-	// 	enc.Encode(KeyValue{key, reduceF(...)})
-	// }
-	// file.Close()
-	//
 	// Your code here (Part I).
 	//
+	tmpMap := make(map[string][]string)
+	var kv KeyValue
+	for i:=0;i<nMap;i++{
+		fileName := reduceName(jobName, i, reduceTask)
+		file, err := os.Open(fileName)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		enc := json.NewDecoder(file)
+
+		err = enc.Decode(&kv)
+
+		for err == nil{
+			_, ok := tmpMap[kv.Key]
+			if !ok {
+				tmpMap[kv.Key] = []string{kv.Value}
+			} else {
+				tmpMap[kv.Key] = append(tmpMap[kv.Key], kv.Value)
+			}
+			err = enc.Decode(&kv)
+		}
+		file.Close()
+	}
+
+	var keys []string
+	for k, _ := range tmpMap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	file1, _ := os.Create(outFile)
+	defer file1.Close()
+	enc1 := json.NewEncoder(file1)
+	//for key := tmpList {
+	//	enc1.Encode(KeyValue{key, reduceF(...)})
+	//}
+	for _, k := range keys {
+		enc1.Encode(KeyValue{k, reduceF(k, tmpMap[k])})
+	}
 }
